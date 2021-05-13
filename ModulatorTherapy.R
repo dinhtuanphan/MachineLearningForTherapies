1369ecffc312a33b07b1e91645798a53dfbb84ca
dinhtuanphan <phandinhtuans@gmail.com>
2021-05-13 18:03:04 -0400

    Add files

--- ModulatorTherapy.R
+++ ModulatorTherapy.R
@@ -6,6 +6,7 @@ library(ggplot2)
 readExcel <- function(filename, tibble = FALSE){
   sheets <- readxl::excel_sheets(filename)
   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  lapply(x, function(x) x[, colSums(is.na(x)) == 0])
   if(!tibble) x <- lapply(x, as.data.frame)
   names(x) <- sheets
   x
@@ -16,5 +17,5 @@ modulator <- readExcel("20210513_updated_healthy_iontophoresis.xlsx")
 
 # Basic exploratory data analysis (EDA) and visualization
 
-ggplot(modulator[[1]], aes(x=SR, y = C_predose_smooth_2min) ) + 
+p <- ggplot(modulator[[1]], aes(x=SR, y = C_predose_smooth_2min) ) + 
     geom_point(size=2, shape=19)
