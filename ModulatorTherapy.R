2cdfc4c3e700b7ae7f8dfd6192efa9b0f78626ce
dinhtuanphan <phandinhtuans@gmail.com>
2021-05-13 19:10:11 -0400

    Add new files

--- ModulatorTherapy.R
+++ ModulatorTherapy.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(dplyr)
 library(ggplot2)
+library(cowplot)
 
 # Function to read all the data in excel sheets
 readExcel <- function(filename, tibble = FALSE){
@@ -17,5 +18,15 @@ modulator <- readExcel("20210513_updated_healthy_iontophoresis.xlsx")
 
 # Basic exploratory data analysis (EDA) and visualization
 
-p <- ggplot(modulator[[1]], aes(x=SR, y = C_predose_smooth_2min) ) + 
+# Plotting function
+myplot <- function(i){
+  ggplot(modulator[[i]], aes(x=SR, y = C_predose_smooth_2min) ) + 
     geom_point(size=2, shape=19)
+}
+
+# Create list of plots
+plist <- lapply(1:12, myplot)
+
+# Plot all together with cowplot
+cowplot::plot_grid(plotlist = plist, align = "hv")
+
