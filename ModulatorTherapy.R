29f9a8d28720f39a21dc52115cf087a542369cc6
dinhtuanphan <phandinhtuans@gmail.com>
2021-05-13 17:00:53 -0400

    Add files

--- ModulatorTherapy.R
+++ ModulatorTherapy.R
@@ -1,5 +1,7 @@
 library(readxl)
 library(ggplot2)
+
+# Function to read all the data in excel sheets
 readExcel <- function(filename, tibble = FALSE){
   sheets <- readxl::excel_sheets(filename)
   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
@@ -8,4 +10,10 @@ readExcel <- function(filename, tibble = FALSE){
   x
 }
 
+# Read data in all sheets
 modulator <- readExcel("20210513_updated_healthy_iontophoresis.xlsx")
+
+# Basic exploratory data analysis (EDA) and visualization
+
+ggplot(modulator[[1]], aes(x=SR, y = C_predose_smooth_2min) ) + 
+    geom_point(size=2, shape=19)
